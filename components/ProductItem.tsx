e3e2ae41827d66d577e90d97a094e892932f98d5
unknown <pduartesilva2005@gmail.com>
2021-08-03 11:15:12 -0300

    App

---
+++ components/ProductItem.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/display-name */
+import { memo, useState } from 'react';
+import dynamic from 'next/dynamic';
+import lodash from 'lodash';
+
+import { AddProductToWishlistProps } from './AddProductToWishlist';
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
+  () => {
+    return import('./AddProductToWishlist').then(
+      mod => mod.AddProductToWishlist
+    );
+  },
+  {
+    loading: () => <span>Carregando...</span>
+  }
+);
+
+interface ProductItemProps {
+  product: {
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  };
+  onAddToWishlist: (id: number) => void;
+}
+
+function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+  const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
+  return (
+    <div>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => setIsAddingToWishlist(true)}>
+        Adicionar aos favoritos
+      </button>
+      {isAddingToWishlist && (
+        <AddProductToWishlist
+          onAddToWishlist={() => onAddToWishlist(product.id)}
+          onRequestClose={() => setIsAddingToWishlist(false)}
+        />
+      )}
+    </div>
+  );
+}
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return lodash.isEqual(prevProps.product, nextProps.product);
+  }
+);
---
+++ components/SearchResults.tsx
@@ -0,0 +1,42 @@
+import { List, ListRowRenderer } from "react-virtualized";
+
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+  totalPrice: number;
+  results: Array<{
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  }>;
+  onAddToWishlist: (id: number) => void;
+}
+
+export function SearchResults({ totalPrice, results, onAddToWishlist }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem 
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    )
+  };
+
+  return (
+    <div>
+      <h2>{totalPrice}</h2>
+
+      <List 
+        height={500}
+        rowHeight={25}
+        width={900}
+        overscanRowCount={5}
+        rowCount={5}
+        rowRenderer={rowRenderer}
+      />
+    </div>
+  )
+}
